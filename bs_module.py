be2c8172a0be660f6f8bcc94ff483e7d8afc7c0c
Phillip Gray <escapethegray11235@gmail.com>
2015-04-29 01:36:29 -0400

    Added functions. Error in lines 99-102 in bs_module.
    There is a pain in the ass error that causes the generate_location function to return a blank list of tuples when ship_direction == "left". I'm not sure why, but it's too late to try and fix it now. Other directions work perfectly. Ugh.

--- bs_module.py
+++ bs_module.py
@@ -1,49 +1,124 @@
-from random import choice, randint
-# define ship class
+from random import choice
+# define classes
+
 
 class Ship(object):
-	"""
-	docstring for Ship class
-	each ship has the following attributes
-	ship_type: str
-	size: int
-	location: list of tuples, of int size
-	"""
-	def __init__(self, ship_type, size, location):
-		super(Ship, self).__init__()
-		self.ship_type = ship_type
-		self.size = size
-		self.location = location
+    """
+    docstring for Ship class
+    each ship has the following attributes
+    ship_type: str
+    size: int
+    location: list of tuples, of int size
+    """
+    def __init__(self, ship_type, size, location):
+        super(Ship, self).__init__()
+        self.ship_type = ship_type
+        self.size = size
+        self.location = location
 
 
 # dict of ships
 
 ship_options = {
-	"aircraft carrier": 5,
-	"battleship": 4,
-	"submarine": 3,
-	"destroyer": 3,
-	"patrol boat": 2,
+    "aircraft carrier": 5,
+    "battleship": 4,
+    "submarine": 3,
+    "destroyer": 3,
+    "patrol boat": 2,
 }
 
 # helper functions
 
-def board_fill(board_size):
-	board = []
-	first_col = []
-	horiz_key = "+abcdefghij"
 
-	for row in range(board_size + 1):
-		if row == 0:
-			for char in horiz_key[0:board_size + 1]:
-				first_col.append(char)
-			board.append(first_col)
-		else:
-			this_row = [str(row), ]
-			this_row.extend(["O"] * board_size)
-			board.append(this_row)
-	return board
+def board_fill(board_size):
+    board = []
+    first_col = []
+    horiz_key = "+abcdefghij"
+
+    for row in range(board_size + 1):
+        if row == 0:
+            for char in horiz_key[0:board_size + 1]:
+                if char == '+':
+                    first_col.append(" " + char)  # this if/then is to fix spacing
+                else:
+                    first_col.append(char)
+            board.append(first_col)
+        else:
+            if row <= 9:  # this if/then is to fix spacing
+                this_row = [" " + str(row), ]
+            else:
+                this_row = [str(row), ]
+            this_row.extend(["O"] * board_size)
+            board.append(this_row)
+    return board
+
 
 def print_board(board):
-	for row in board:
-		print " ".join(row)
+    for row in board:
+        print " ".join(row)
+
+
+def generate_position_list(board_size):
+    '''
+    function takes board size as int
+    returns list of tuples, where
+    (row, column)
+    '''
+    position_list = []
+    for row in range(1, board_size + 1):
+        for column in range(1, board_size + 1):
+            position_list.append((row, column), )
+    # print position_list
+    return position_list
+
+
+def ship_start_space(available_space_list):
+    '''
+    takes a list of tuple coordinates and
+    returns a random choice from the list
+    '''
+    start_coord = choice(available_space_list)
+    return start_coord
+
+
+def ship_direction():
+    '''
+    returns a random direction
+    '''
+    return choice(["right", "left", "up", "down"])
+
+def generate_location(size, board_list, other_ships_list):
+    location = []
+    direction = ship_direction()
+    start_coord = ship_start_space(board_list)
+    if direction == "right":
+        location.append(start_coord)
+        for col_num in range(start_coord[1] + 1, start_coord[1] + size):
+            location.append((start_coord[0], col_num))
+
+    elif ship_direction == "left":
+        location.append(start_coord)
+        for col_num in range(start_coord[1] - 1, start_coord[1] - size, -1):
+            location.append((start_coord[0], col_num))
+
+    elif direction == "down":
+        location.append(start_coord)
+        for row_num in range(start_coord[0] + 1, start_coord[0] + size):
+            location.append((row_num, start_coord[1]))
+
+    elif direction == "up":
+        location.append(start_coord)
+        for row_num in range(start_coord[0] - 1, start_coord[0] - size, -1):
+            location.append((row_num, start_coord[1]))
+
+    print start_coord
+    print direction
+    print location
+
+    for points in location:
+        if points not in board_list or points in other_ships_list:
+            print "nope"
+            return generate_location(size, board_list, other_ships_list)
+
+    return location
+
