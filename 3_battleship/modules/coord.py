f69ede1b62cbacda9511e0363e0bf5b2e27acd51
phillipgray <escapethegray11235@gmail.com>
2015-07-31 18:19:04 +0000

    adds new class gridcoord

---
+++ 3_battleship/modules/coord.py
@@ -0,0 +1,11 @@
+import re
+
+class GridCoord(object):
+    """
+    This object takes a two character string and converts and stores it
+    as a (column, row) tuple that corresponds to a square on the board.
+    ([a-j][1-10])
+    """
+    def __init__(self):
+        pass
+    
